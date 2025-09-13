7281d05d83fbe7ae909ce992e7d5c83ee2dfd881
tmd <minhduongqo@gmail.com>
2025-09-14 00:22:12 +0200

    fix fastmcp cloud deployment attemp#1

--- mcp_server.py
+++ mcp_server.py
@@ -5,23 +5,23 @@ from tools.monthly_report import get_monthly_report
 from starlette.requests import Request
 from starlette.responses import PlainTextResponse
 
-app = FastMCP("ExpenseTrackerMCP",json_response=True,stateless_http=True)
+mcp = FastMCP("ExpenseTrackerMCP",json_response=True,stateless_http=True)
 
-@app.custom_route("/health", methods=["GET"])
+@mcp.custom_route("/health", methods=["GET"])
 async def health_check(request: Request) -> PlainTextResponse:
     return PlainTextResponse("OK")
 
-@app.tool(name="add_purchase", description="Add a new expense to Google Sheet")
+@mcp.tool(name="add_purchase", description="Add a new expense to Google Sheet")
 def add_purchase_tool(date: str, category: str, description: str, description_vi: str, fee: float, currency: str):
     return add_purchase(date, category, description, description_vi, fee, currency)
 
-@app.tool(name="get_monthly_report", description="Get a monthly expense summary")
+@mcp.tool(name="get_monthly_report", description="Get a monthly expense summary")
 def get_monthly_report_tool(year: int, month: int):
     return get_monthly_report(year, month)
 
-# if __name__ == "__main__":
-#     # Expose as HTTP server
-#     mcp.run(transport="http")
+if __name__ == "__main__":
+    # Expose as HTTP server
+    mcp.run(transport="http")
 
 # Just run: fastmcp run mcp_server.py:mcp --host 127.0.0.1 --port 8000 --transport http
 
